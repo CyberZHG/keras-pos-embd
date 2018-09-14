a19c55b19cf4f7b5af77269f6705150bc6a6700c
CyberZHG <CyberZHG@gmail.com>
2018-09-15 07:09:47 +0800

    Initialize framework

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# keras-pos-embd
-Position embedding layers in Keras
+# Keras Piecewise
+
+[![Travis](https://travis-ci.org/CyberZHG/keras-pos-embd.svg)](https://travis-ci.org/CyberZHG/keras-pos-embd)
+[![Coverage](https://coveralls.io/repos/github/CyberZHG/keras-pos-embd/badge.svg?branch=master)](https://coveralls.io/github/CyberZHG/keras-pos-embd)
+[![PyPI](https://img.shields.io/pypi/pyversions/keras-pos-embd.svg)](https://pypi.org/project/keras-pos-embd/)
+
+Position embedding layers in Keras.
+
+## Install
+
+```bash
+pip install keras-pos-embd
+```
+
+## Usage
+
+```python
+```
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='keras-pos-embd',
+    version='0.1',
+    packages=['keras_pos_embd'],
+    url='https://github.com/CyberZHG/keras-pos-embd',
+    license='MIT',
+    author='CyberZHG',
+    author_email='CyberZHG@gmail.com',
+    description='Position embedding layers in Keras',
+    long_description=open('README.rst', 'r').read(),
+    install_requires=[
+        'numpy',
+        'keras',
+    ],
+    classifiers=(
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
