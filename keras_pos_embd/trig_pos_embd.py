15b14e4a264334c49da8882c733bed637c4540cd
CyberZHG <CyberZHG@gmail.com>
2018-10-03 12:14:12 +0800

    Add concat mode

--- keras_pos_embd/trig_pos_embd.py
+++ keras_pos_embd/trig_pos_embd.py
@@ -16,13 +16,21 @@ class TrigPosEmbedding(keras.layers.Layer):
 
     Add mode:
         # Input shape
-            2D tensor with shape: `(batch_size, sequence_length, output_dim)`.
+            3D tensor with shape: `(batch_size, sequence_length, feature_dim)`.
 
         # Output shape
-            3D tensor with shape: `(batch_size, sequence_length, output_dim)`.
+            3D tensor with shape: `(batch_size, sequence_length, feature_dim)`.
+
+    Concat mode:
+        # Input shape
+            3D tensor with shape: `(batch_size, sequence_length, feature_dim)`.
+
+        # Output shape
+            3D tensor with shape: `(batch_size, sequence_length, feature_dim + output_dim)`.
     """
     MODE_EXPAND = 'expand'
     MODE_ADD = 'add'
+    MODE_CONCAT = 'concat'
 
     def __init__(self,
                  mode=MODE_ADD,
@@ -32,9 +40,9 @@ class TrigPosEmbedding(keras.layers.Layer):
         :param output_dim: The embedding dimension.
         :param kwargs:
         """
-        if mode == self.MODE_EXPAND:
+        if mode in [self.MODE_EXPAND, self.MODE_CONCAT]:
             if output_dim is None:
-                raise NotImplementedError('`output_dim` is required in `expand` mode')
+                raise NotImplementedError('`output_dim` is required in `%s` mode' % mode)
             if output_dim % 2 != 0:
                 raise NotImplementedError('It does not make sense to use an odd output dimension: %d' % output_dim)
         self.mode = mode
@@ -56,12 +64,17 @@ class TrigPosEmbedding(keras.layers.Layer):
     def compute_output_shape(self, input_shape):
         if self.mode == self.MODE_EXPAND:
             return input_shape + (self.output_dim,)
+        if self.mode == self.MODE_CONCAT:
+            return input_shape[:-1] + (input_shape[-1] + self.output_dim,)
         return input_shape
 
     def call(self, inputs, mask=None):
         input_shape = K.shape(inputs)
         if self.mode == self.MODE_ADD:
             batch_size, seq_len, output_dim = input_shape[0], input_shape[1], input_shape[2]
+            pos_input = K.tile(K.expand_dims(K.arange(seq_len), axis=0), [batch_size, 1])
+        elif self.mode == self.MODE_CONCAT:
+            batch_size, seq_len, output_dim = input_shape[0], input_shape[1], self.output_dim
             pos_input = K.tile(K.expand_dims(K.arange(seq_len), axis=0), [batch_size, 1])
         else:
             output_dim = self.output_dim
@@ -89,6 +102,8 @@ class TrigPosEmbedding(keras.layers.Layer):
         )
         embd = K.stack([even_embd, odd_embd], axis=-1)
         output = K.reshape(embd, [-1, K.shape(inputs)[1], output_dim])
+        if self.mode == self.MODE_CONCAT:
+            output = K.concatenate([inputs, output], axis=-1)
         if self.mode == self.MODE_ADD:
             output += inputs
         return output
--- tests/test_trig_pos_embd.py
+++ tests/test_trig_pos_embd.py
@@ -53,11 +53,11 @@ class TestSinCosPosEmbd(unittest.TestCase):
 
     def test_add(self):
         seq_len = random.randint(1, 10)
-        embd_dim = random.randint(1, 20) * 2
-        inputs = np.ones((1, seq_len, embd_dim))
+        embed_dim = random.randint(1, 20) * 2
+        inputs = np.ones((1, seq_len, embed_dim))
         model = keras.models.Sequential()
         model.add(TrigPosEmbedding(
-            input_shape=(seq_len, embd_dim),
+            input_shape=(seq_len, embed_dim),
             mode=TrigPosEmbedding.MODE_ADD,
             name='Pos-Embd',
         ))
@@ -69,11 +69,40 @@ class TestSinCosPosEmbd(unittest.TestCase):
         predicts = model.predict(inputs)[0].tolist()
         sess = tf.Session()
         for i in range(seq_len):
-            for j in range(embd_dim):
+            for j in range(embed_dim):
                 actual = predicts[i][j]
                 if j % 2 == 0:
-                    expect = 1.0 + tf.sin(i / tf.pow(10000.0, float(j) / embd_dim))
+                    expect = 1.0 + tf.sin(i / tf.pow(10000.0, float(j) / embed_dim))
                 else:
-                    expect = 1.0 + tf.cos(i / tf.pow(10000.0, (j - 1.0) / embd_dim))
+                    expect = 1.0 + tf.cos(i / tf.pow(10000.0, (j - 1.0) / embed_dim))
                 expect = expect.eval(session=sess)
-                self.assertAlmostEqual(expect, actual, places=6, msg=(embd_dim, i, j, expect, actual))
+                self.assertAlmostEqual(expect, actual, places=6, msg=(embed_dim, i, j, expect, actual))
+
+    def test_concat(self):
+        seq_len = random.randint(1, 10)
+        feature_dim = random.randint(1, 20)
+        embed_dim = random.randint(1, 20) * 2
+        inputs = np.ones((1, seq_len, feature_dim))
+        model = keras.models.Sequential()
+        model.add(TrigPosEmbedding(
+            input_shape=(seq_len, feature_dim),
+            output_dim=embed_dim,
+            mode=TrigPosEmbedding.MODE_CONCAT,
+            name='Pos-Embd',
+        ))
+        model.compile('adam', keras.losses.mae, {})
+        model_path = os.path.join(tempfile.gettempdir(), 'test_trig_pos_embd_%f.h5' % random.random())
+        model.save(model_path)
+        model = keras.models.load_model(model_path, custom_objects={'TrigPosEmbedding': TrigPosEmbedding})
+        model.summary()
+        predicts = model.predict(inputs)[0].tolist()
+        sess = tf.Session()
+        for i in range(seq_len):
+            for j in range(embed_dim):
+                actual = predicts[i][feature_dim + j]
+                if j % 2 == 0:
+                    expect = tf.sin(i / tf.pow(10000.0, float(j) / embed_dim))
+                else:
+                    expect = tf.cos(i / tf.pow(10000.0, (j - 1.0) / embed_dim))
+                expect = expect.eval(session=sess)
+                self.assertAlmostEqual(expect, actual, places=6, msg=(embed_dim, i, j, expect, actual))
