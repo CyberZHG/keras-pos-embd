830c48d9b4053bae9b1c4d1adbf481923b1e64b7
CyberZHG <CyberZHG@gmail.com>
2019-01-31 09:14:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
+import codecs
 from setuptools import setup, find_packages
+
+
+with codecs.open('README.md', 'r', 'utf8') as reader:
+    long_description = reader.read()
+
+
+with codecs.open('requirements.txt', 'r', 'utf8') as reader:
+    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+
 
 setup(
     name='keras-pos-embd',
-    version='0.6.0',
+    version='0.7.0',
     packages=find_packages(),
     url='https://github.com/CyberZHG/keras-pos-embd',
     license='MIT',
     author='CyberZHG',
     author_email='CyberZHG@gmail.com',
     description='Position embedding layers in Keras',
-    long_description=open('README.rst', 'r').read(),
-    install_requires=[
-        'numpy',
-        'keras',
-    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
     classifiers=(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
